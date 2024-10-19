f64f60dc01ca1d4dc926c42fdd83a79172c70c52
radhabaran <mohantyradha@yahoo.co.in>
2024-10-20 01:15:34 +0530

    1st change updated pip install

--- Leveraging_LLMs_for_Querying_Insights.py
+++ Leveraging_LLMs_for_Querying_Insights.py
@@ -1,12 +1,12 @@
 # Install Dependencies
 # %%capture
-# !pip -q install openai
-# !pip -q install langchain-openai
-# !pip -q install langchain-core
-# !pip -q install langchain-community
-# !pip -q install sentence-transformers
-# !pip -q install langchain-huggingface
-# !pip -q install langchain_experimental
+# pip -q install openai
+# pip -q install langchain-openai
+# pip -q install langchain-core
+# pip -q install langchain-community
+# pip -q install sentence-transformers
+# pip -q install langchain-huggingface
+# pip -q install langchain_experimental
 # ************************************************************************************************
 # Import required packages
 # ************************************************************************************************
